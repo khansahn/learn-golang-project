d16b750378733e735751b65ff3a10b8ad989bb33
khansahn <snabilakhansa@gmail.com>
2019-05-20 16:30:52 +0700

    crud ke database blm jadi API

--- hello/hello.go
+++ hello/hello.go
@@ -16,6 +16,16 @@ const (
 	dbname = "inksnippet"
 )
 
+type CodeSnippet struct {
+	ID	int
+	Title	string
+	Codelines 	string
+	Tag 	string
+	Author 	string
+	ContentType 	string
+	CreatedAt 	string
+	UpdatedAt 	string
+}
 
 func main() {
     fmt.Printf("hello, world\n")
@@ -55,6 +65,7 @@ func main() {
 */
 
     // UPDATE
+    /*
     sqlStatementUpdate := `
     UPDATE codesnippet
     SET title=$2, codelines=$3, tag=$4, author=$5, content_type=$6, updated_at=$7
@@ -73,5 +84,79 @@ func main() {
     }
     fmt.Println(count)
     */
-    fmt.Println(id)
+    //fmt.Println(id)
+    
+
+    // GET
+    /*
+    sqlStatementGet := `
+    SELECT id, title
+    FROM codesnippet
+    WHERE id=$1;
+    `
+    var id int
+    var title string
+    row := db.QueryRow(sqlStatementGet, 3)
+    switch err = row.Scan(&id, &title); err {
+    case sql.ErrNoRows:
+	    fmt.Println("no rows returned??>?>?")
+    case nil:
+	    fmt.Println(id, title)
+    default:
+	    panic(err)
+    }
+    */
+
+    /*
+    // multiple GET
+    sqlStatementGet2 := `
+    SELECT * FROM codesnippet WHERE id=$1
+    `
+    var codesnippet CodeSnippet
+    row2 := db.QueryRow(sqlStatementGet2, 1)
+    err = row2.Scan(&codesnippet.ID, &codesnippet.Title, &codesnippet.Codelines, &codesnippet.Tag, &codesnippet.Author, &codesnippet.ContentType, &codesnippet.CreatedAt, &codesnippet.UpdatedAt)
+    switch err {
+    case sql.ErrNoRows:
+	    fmt.Println("gggada")
+	    return
+    case nil:
+	    fmt.Println(codesnippet)
+    default:
+	    panic(err)
+    }
+    */
+    // MULTIPLE GET
+    rows, err := db.Query("SELECT id, title, tag, created_at FROM codesnippet")
+    if err != nil {
+    	panic(err)
+    }
+    defer rows.Close()
+    for rows.Next() {
+    	var id int
+	var title string
+	var tag string
+	var createdAt string
+	err = rows.Scan(&id, &title, &tag, &createdAt)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(id, title, tag, createdAt)
+    }
+    // get error encountered during iteration
+    err = rows.Err()
+    if err != nil {
+    	panic(err)
+    }
+
+    /*
+    // DELETE
+    sqlStatementDelete := `
+    DELETE FROM codesnippet
+    WHERE id=$1;
+    `
+    _, err = db.Exec(sqlStatementDelete, 4)
+    if err != nil {
+    	panic(err)
+    }
+    */
 }
